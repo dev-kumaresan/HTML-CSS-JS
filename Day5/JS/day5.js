4b88f001d2b7d33c331c3d8594f98b70cb72ad64
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-18 11:03:20 +0530

    updated

--- Day5/JS/day5.js
+++ Day5/JS/day5.js
@@ -5,7 +5,12 @@ counters.forEach(counter => {
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
         const increment = target / 200
-        if(c<target)
+        if(c<target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter,1)
+        }else{
+            counter.innerText = target
+        }
 
     }
 })
