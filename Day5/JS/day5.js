5f235cc68efdc92775c91d2e13de88dce9eb08d3
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-18 11:00:30 +0530

    updated

--- Day5/JS/day5.js
+++ Day5/JS/day5.js
@@ -1,4 +1,11 @@
 const counters = document.querySelectorAll('.counter')
 counters.forEach(counter => {
     counter.innerText = '0'
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+        const increment = target / 200
+        if(c<target)
+
+    }
 })
