699fbf69ea844f3fedf83ab9d24d4a99bc6857da
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-20 13:39:01 +0530

    images added

--- Day7/JS/day7.js
+++ Day7/JS/day7.js
@@ -1,14 +1,16 @@
 const contents = document.querySelectorAll('.content')
 const listItems = document.querySelectorAll('nav ul li')
-listItems.forEach((items,idx)=>{
-    items.addEventListener('click',()=>{
+listItems.forEach((item,    idx)=>{
+    item.addEventListener('click',()=>{
         hideAllContents()
         hideAllItems()
+        item.classList.add('active')
+        contents[idx].classList.add('show')
     })
 })
 function hideAllContents(){
     contents.forEach(content=>content.classList.remove('show'))
 }
-function hideAllContents(){
-    contents.forEach(content=>content.classList.remove('show'))
+function hideAllItems(){
+    listItems.forEach(item=>item.classList.remove('active'))
 }
