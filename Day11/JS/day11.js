20881edfca0738773b399a433077e4feb42f6175
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-25 14:30:35 +0530

    JS updated

---
+++ Day11/JS/day11.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let size =20
+let color = 'black'
+let x 
+let y
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
+})
+function drawCircle(x,y){
+    
+    ctx.beginPath();
+    ctx.arc(75,75,50,0,Math.PI*2,true)
+    ctx.fillStyle = color
+    ctx.fill()
+}
+function drawLine(x1, y1, x2, y2){
+    
+    ctx.beginPath()
+    ctx.moveTo(x1,y1)
+    ctx.lineTo(x2,y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth - size
+    ctx.stroke()
+}
+// drawCircle(100,200)
+// drawLine(300, 300, 300, 500)
