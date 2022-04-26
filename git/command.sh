896f78f9d707428a2f153d76dfa5b12b76730dec
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-26 16:12:45 +0530

    commit

--- git/command.sh
+++ git/command.sh
@@ -1,10 +1,4 @@
-cm='nk'
 git add .
 git push
 read -p "Enter commit Message : " commit_message
-if [commit_message -eq cm] 
-then
-  git commit -m "Auto-commit"
-else
 git commit -m "$commit_message"
-fi
