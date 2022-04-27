c72aa7107fa4756de0d534bfc1571892b03d6560
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-27 14:01:02 +0530

    updated

--- Day13/JS/day13.js
+++ Day13/JS/day13.js
@@ -1,3 +1,23 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    addTodo() 
+})
+function addTodo(){
+    let todoText = input.value
+    if(todo){
+            todoText = todo.text
+    }
+    if(todoText){
+        const todoEl = document.createElement('li')
+        if(todo && todo.completed){
+            todoEl.classList.add('completed')
+        }
+        todoEl.innerText = todoText
+        todoEl.addEventListener('click',()=>todoEl.classList.toggle('completed'))
+        todosUL.appendChild(todoEl)
+        input.value=''
+    }
+}
