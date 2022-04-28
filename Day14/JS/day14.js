2b2cb50f9b64f72c1e3a8dcf327c479a8e2962d9
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-28 15:05:42 +0530

    updated

--- Day14/JS/day14.js
+++ Day14/JS/day14.js
@@ -1,4 +1,8 @@
 const addBtn = document.getElementById('add')
+const notes = JSON.parse(localStorage.getItem('notes'))
+if(notes){
+    notes.forEach(note=>addNewNote(note))
+}
 addBtn.addEventListener('click',()=>addNewNote())
 function addNewNote(text = ''){
     const note = document.createElement('div')
@@ -16,15 +20,26 @@ function addNewNote(text = ''){
     const main = note.querySelector('.main')
     const textArea = note.querySelector('textarea')
     textArea.value = text
-    main.innerHTML = marked(text)
+    main.innerHTML = text
     deleteBtn.addEventListener('click',()=>{
         note.remove()
+        updateLS()
     })
     editBtn.addEventListener('click',()=>{
         main.classList.toggle('hidden')
         textArea.classList.toggle('hidden')
 
     })
-    textArea
+    textArea.addEventListener('input',(e)=>{
+       const { value } =  e.target
+       main.innerHTML = value
+       updateLS()
+    })
     document.body.appendChild(note)
+}
+function updateLS(){
+  const notesText = document.querySelectorAll('textarea')
+  const notes  = []
+  notesText.forEach(note => notes.push(note.value))
+  localStorage.setItem('notes',JSON.stringify(notes))
 }
