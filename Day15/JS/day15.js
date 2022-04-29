863b2ce1e18a2a0fa6f071c6930570edc5ab439a
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-29 15:44:41 +0530

    updated

---
+++ Day15/JS/day15.js
@@ -0,0 +1,13 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+let activeSlide = 0
+setBgToBody()
+function setBgToBody(){
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+function setActiveSLide(){
+    slides.forEach(slide=>slide.classList.remove('active'))
+    slides[activeSlides]
+}
